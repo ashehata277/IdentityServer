e65131b73628e49259cee0f4d526fe2cef117956
ahmed.shehata <ahmed.shehata@hadafsolutions.com>
2022-12-03 13:42:41 +0200

    -

--- IdentityServer/IdentityServer4Configuration/IDentityAppSettings.cs
+++ IdentityServer/IdentityServer4Configuration/IDentityAppSettings.cs
@@ -1,9 +1,4 @@
 ﻿using IdentityServer.Helper;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace IdentityServer.IdentityServer4Configuration
 {
