65a188015e453cc299946f00dedc42540039bb3b
ahmed.shehata <ahmed.shehata@hadafsolutions.com>
2022-12-03 13:39:40 +0200

    -

--- IdentityServer/Shared/SharedLogic/IdentityServer/Role.cs
+++ IdentityServer/Shared/SharedLogic/IdentityServer/Role.cs
@@ -4,6 +4,5 @@ namespace SharedLogic.IdentityServer
 {
     public class Role : IdentityRole<string>
     {
-        public override string Id { get => base.Id; set => base.Id = Guid.NewGuid().ToString(); }
     }
 }
