46fb56d146e77a2f0e997cf65b023d7c6be75aaa
ahmed.shehata <ahmed.shehata@hadafsolutions.com>
2022-12-20 18:13:01 +0200

    -

--- IdentityServer/Shared/SharedLogic/IdentityServer/IdentityConstants.cs
+++ IdentityServer/Shared/SharedLogic/IdentityServer/IdentityConstants.cs
@@ -11,17 +11,17 @@ namespace SharedLogic.IdentityServer
         public static readonly int TokenLifeTime = 3600;
         public static readonly int RefreshTokenLifeTime = 2592000;
         public static readonly int TokenCleanupInterval = 2592000;
-        public static readonly string ApiResource= "Api";
-        public static readonly string MVCResource= "MVC";
-        public static readonly string AdminUserName= "admin";
+        public static readonly string ApiResource = "Api";
+        public static readonly string MVCResource = "MVC";
+        public static readonly string AdminUserName = "admin";
         public static readonly string AdminPassword = "admin";
-        public static readonly string AdminUserId= "32efec81-d676-4e7a-b3c1-12984c515bf2";
-        public static readonly string AdminRoleName= "admin";
+        public static readonly string AdminUserId = "32efec81-d676-4e7a-b3c1-12984c515bf2";
+        public static readonly string AdminRoleName = "admin";
         public static readonly string AdminRoleId = "8ce88938-6386-40ce-ab79-16306e3991a8";
-        public static readonly string  AdminPhoneNumber= "admin";
-        public static readonly string  AdminPhoneEmail= "admin@admin.com";
-        public static readonly string  TokenInfo_Name= "username";
-        public static readonly string  TokenInfo_AuthenticationMethod= "custom";
+        public static readonly string AdminPhoneNumber = "admin";
+        public static readonly string AdminPhoneEmail = "admin@admin.com";
+        public static readonly string TokenInfo_Name = "username";
+        public static readonly string TokenInfo_AuthenticationMethod = "custom";
         public static readonly string TokenInfo_IdentityProvider = "local";
         public static readonly string SecurityStamp = "SecurityStamp";
 
@@ -39,10 +39,10 @@ namespace SharedLogic.IdentityServer
         public static readonly string SwaggerScope = "swagger-Client-Scope";
 
         //-------------------- mobile Client ----------------------------------------
-        public static readonly string MobileClientSecret= "83906b4d-72b6-4acf-9a6d-cdcf546c3f33";
-        public static readonly string MobileClientId= "bea8cbfe-188e-4d5b-ad50-f2c2df46971d";
-        public static readonly string ApiResourceSecret= "db62d929-8f78-4c80-b9dd-8d9c9ec86e77";
-        public static readonly string MVCResourceSecret= "ccba8b8c-d866-4859-87ef-e36ca70aa86c";
+        public static readonly string MobileClientSecret = "83906b4d-72b6-4acf-9a6d-cdcf546c3f33";
+        public static readonly string MobileClientId = "bea8cbfe-188e-4d5b-ad50-f2c2df46971d";
+        public static readonly string ApiResourceSecret = "db62d929-8f78-4c80-b9dd-8d9c9ec86e77";
+        public static readonly string MVCResourceSecret = "ccba8b8c-d866-4859-87ef-e36ca70aa86c";
 
 
 
